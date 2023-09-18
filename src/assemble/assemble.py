7a2e3463130515f47d631594c9d2eee49eec4522
AUnicornWithNoLife <55228370+AUnicornWithNoLife@users.noreply.github.com>
2023-09-18 11:14:03 +0000

    a

--- src/assemble/assemble.py
+++ src/assemble/assemble.py
@@ -1,5 +1,33 @@
 def assemble(fin: str, fout: str) -> None:
-    print(fin, fout)
+    pointers = {'_START': 0}
+
+    file_in = open(fin, "r").read()
+
+instructions = {
+    'lda': (0, ("ram", "reg")),
+    'sta': (1, ("reg", "ram")),
+    'mov': (2, ("reg", "reg")),
+    'add': (3, ("reg", "reg")),
+    'sub': (4, ("reg", "reg")),
+    'mul': (5, ("reg", "reg")),
+    'div': (6, ("reg", "reg")),
+    'out': (7, ("reg", "none")),
+    'jmp': (8, ("ram", "none")),
+    'jnz': (9, ("reg", "ram")),
+    'stp': (10, ("none", "none"))
+}
+
+def cleanup(sin: str) -> str:
+    ret = ""
+
+    for i in range(len(sin)):
+        if (str[i] == '\n'):
+            pass
+
+        a = str[i]
+
+        for ia in range(len(a)):
+            
 
 if __name__ == "__main__":
     print("DO NOT DIRECT RUN ASSEMBLE.PY, RUN MAIN.PY INSTEAD")
