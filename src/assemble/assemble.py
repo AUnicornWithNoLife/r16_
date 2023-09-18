059966a43f39a74d451f767583470c160d4efca5
AUnicornWithNoLife <55228370+AUnicornWithNoLife@users.noreply.github.com>
2023-09-18 13:03:14 +0100

    fin py

--- src/assemble/assemble.py
+++ src/assemble/assemble.py
@@ -1,9 +1,67 @@
+import struct
+
 def assemble(fin: str, fout: str) -> None:
     pointers = {'_START': 0}
 
     file_in = open(fin, "r").read()
 
-    print(cleanup(file_in))
+    f_clean = cleanup(file_in)
+
+    for i in range(len(f_clean)):
+        if f_clean[i][0] == '_':
+            pointers.update({f_clean[i].split(" ")[0]: i})
+
+    ram = []
+
+    for i in range(len(f_clean)):
+        f_c_i = f_clean[i].split(" ")
+
+        if (f_c_i[0][0] == "_"):
+            f_c_i = f_c_i[1:len(f_c_i)]
+
+        print(f_c_i)
+
+        if (f_c_i[0] == "val"):
+            ram.append(int(f_c_i[1], 16))
+            ram.append(0)
+            ram.append(0)
+
+            continue
+
+        instruct = instructions[f_c_i[0]]
+
+        ram.append(instruct[0])
+
+        if instruct[1][0] == "none":
+            ram.append(0)
+            ram.append(0)
+
+            continue
+        elif instruct[1][0] == "ram":
+            if f_c_i[1][0] == '_':
+                ram.append(pointers[f_c_i[1]] * 3)
+            else:
+                ram.append(int(f_c_i[1]))
+        elif instruct[1][0] == "reg":
+            ram.append(registers.index(f_c_i[1]))
+
+        if instruct[1][1] == "none":
+            ram.append(0)
+
+            continue
+        elif instruct[1][1] == "ram":
+            if f_c_i[2][0] == '_':
+                ram.append(pointers[f_c_i[2]] * 3)
+            else:
+                ram.append(int(f_c_i[2]))
+        elif instruct[1][1] == "reg":
+            ram.append(registers.index(f_c_i[2]))
+
+    file_out = open(fout, "wb")
+
+    for i in range(len(ram)):
+        a = struct.pack("@B", ram[i])
+        file_out.write(a)
 
 instructions = {
     'lda': (0, ("ram", "reg")),
@@ -19,23 +77,26 @@ instructions = {
     'stp': (10, ("none", "none"))
 }
 
+registers = [
+    "r0",
+    "r1",
+    "inr",
+    "acu",
+    "r0a",
+    "r0b",
+    "r1a",
+    "r1b"
+]
+
 def cleanup(sin: str) -> str:
-    ret = ""
+    tmp = sin.split('\n')
 
-    for i in range(len(sin)):
-        if (str[i] == '\n'):
-            pass
+    for i in range(len(tmp)):
+        tmp[i] = " ".join(tmp[i].split(" "))
 
-        a = str[i]
+    return tmp
 
-        for ia in range(len(a)):
-            if (a[ia] == ' ') & (ret[len(ret) - 1] != ' '):
-                ret += ' '
-            elif is_allowed(a[ia]):
-                ret += a[ia]
-
-
-def is_allowed(c: char) -> bool:
+def is_allowed(c) -> bool:
     if c.isalnum():
         return True
 
--- src/main.c
+++ src/main.c
@@ -11,36 +11,9 @@
 #include "cpu/ram.h"
 #include "cpu/reg.h"
 
-#include "assemble/assemble.h"
-
 int main(int argc, char** argv)
 {
     carg_parse_data* args = carg_parse(argc, argv);
-
-    char *f_n = "./.build/test.asm";
-    char *f = (char*)malloc(4096);
-    FILE *f_p = fopen(f_n, "r");
-
-    fscanf(f_p, "%4095c", f);
-
-    printf("loaded\n");
-
-    uint16_t asm_len = 0;
-    uint8_t *asm_v = assemble(f, true, &asm_len);
-
-    printf("assembled");
-
-    ram_setup();
-    reg_setup();
-
-    for (uint16_t i = 0; i < asm_len; i++)
-    {
-        printf("written %hu of %hu", i, asm_len);
-
-        ram[i] = asm_v[i];
-    }
-
-    
 
     carg_parse_free(args);
 }
